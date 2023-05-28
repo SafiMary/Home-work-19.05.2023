a3aed2a8943812a968680be4710d23f6c01f8e5f
Мария Сафи <safim@yandex.ru>
2023-05-28 20:38:28 +0500

    Add project files.

---
+++ Home work 19.05.2023/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_19._05._2023
+{
+    class CaesarCipher
+    {
+        static string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+       
+
+        public static string Encryption(string text)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (text[i] == alph[j])
+                        res.Append(alph[(j + 3) % alph.Length]);
+
+            return res.ToString();
+        }
+        public static string Decryption(string crypt)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < crypt.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (crypt[i] == alph[j])
+                        res.Append(alph[(j - 3 + alph.Length) % alph.Length]);
+
+            return res.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "SAMURai";
+            string crypt = CaesarCipher.Encryption(text);
+            Console.WriteLine($"Зашифрованное сообщение: {crypt}\n");
+            string decrypt = CaesarCipher.Decryption(crypt);
+            Console.WriteLine($"Расшифрованное сообщение: {decrypt}\n");
+        }
+    }
+}
